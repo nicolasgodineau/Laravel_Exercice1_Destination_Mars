48657ee4cecdd13a6ba08c164ba81f4e216d34cc
nicolasgodineau <nicolasgodineau@live.fr>
2023-03-06 17:37:02 -0500

    Mise à jour d'information sur la mise en marche Changement mineur sur le style

--- MiseEnRoute.md
+++ MiseEnRoute.md
@@ -15,3 +15,9 @@ Pour ce projet, il nous est demander de créer un site web statique avec Laravel
 ## Autres
 
 Les images pour ce projet on etaient créer avec l'IA Midjourney
+
+#### Mise en route
+
+Pour la mise en route faire dans le terminal l'update des packages npm `npm update`
+Puis lancer le serveur Laravel `php artisant serve`
+Si besoin de faire des modifications du style avec Tailwind il faut s'assurer qu'on est bien en "wath" et qu'on compile bien le code avec webpack, dans le terminal faire `npm run dev`
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,40 +5,44 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Revalia&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/4890a1a8f5.js" crossorigin="anonymous"></script>
-    <link href="{{ asset ('css/app.css')}}" rel="stylesheet" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
 
     <title>@yield('title')</title>
 </head>
 
 <body
-    class="bg-repeat bg-contain w-[1440px] h-[100vh] flex flex-col items-center justify-items-center mx-auto my-0  p-7 text-white" style="background-image: url({{ url('assets/img/background.jpg')}});">
-    <main
-    class="bg-no-repeat bg-cover w-[100%] h-[100%] flex flex-col items-center justify-between rounded-[30px] pt-5" style="background-image: url({{ url('assets/img/home.webp')}});">
+    class="bg-repeat bg-contain w-[1440px] h-[100vh] flex flex-col items-center justify-items-center mx-auto my-0  p-7 text-white"
+    style="background-image: url({{ url('assets/img/background.jpg') }});">
     <nav class="flex flex-row items-center w-full px-8" id="mainNav">
         <div class="grow">
-            <a class="flex flex-row gap-2 items-center" href="home"><img class="w-4 invert" src="assets/img/favicon.ico"
-                    alt=""><span class=" font-Revalia text-white">Mars Real Estate</span>
+            <a class="flex flex-row gap-2 items-center" href="home"><img class="w-4 invert"
+                    src="assets/img/favicon.ico" alt=""><span class=" font-Revalia text-white">Mars Real
+                    Estate</span>
             </a>
         </div>
         <div class="flex flex-row gap-8 items-center">
-            <a class=" font-Revalia px-2 py-3 text-white hover:scale-110 transition-all hover:underline" href="houses">Nos
+            <a class=" font-Revalia px-2 py-3 text-white hover:scale-110 transition-all hover:underline"
+                href="houses">Nos
                 maisons</a>
-            <a class=" font-Revalia px-2 py-3 text-white hover:scale-110 transition-all hover:underline" href="about">À
+            <a class=" font-Revalia px-2 py-3 text-white hover:scale-110 transition-all hover:underline"
+                href="about">À
                 propos</a>
             <a class=" font-Revalia px-6 py-2 text-white border-2 rounded-full hover:scale-110 transition-all hover:backdrop-blur-3xl"
                 href="contact">Contact</a>
         </div>
     </nav>
-    <!-- Contenu -->
-    @yield('content')
+    <main class="bg-no-repeat bg-cover w-[100%] h-[90vh] flex flex-col items-center justify-center rounded-[30px] pt-5"
+        style="background-image: url({{ url('assets/img/home.webp') }});">
+        <!-- Contenu -->
+        @yield('content')
 
-    <script src="{{ asset ('js/scripts.js')}}"></script>
-    @yield('Javascript')
+        <script src="{{ asset('js/scripts.js') }}"></script>
+        @yield('Javascript')
 </body>
 
 </html>
